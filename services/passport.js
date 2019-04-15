8c6c32edff5362354169ece26b745f9a6e88c8e1
hanna <hanna.likhtman@gmail.com>
2019-04-15 11:04:44 +0800

    added MongoDB, separated routers, added PassportJS

---
+++ services/passport.js
@@ -0,0 +1,17 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToke, profile) => {
+    new User({ googleId: profile.id }).save();
+  })
+);
+
