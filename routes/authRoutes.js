44bcaae945afad45580211639a36c525e3f9daee
hanna <hanna.likhtman@gmail.com>
2019-04-19 18:47:23 +0800

    finished auth flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 
-module.exports = (app) => {
+module.exports = app => {
   app.get(
     '/auth/google',
     passport.authenticate('google', {
@@ -12,6 +12,21 @@ module.exports = (app) => {
     '/auth/google/callback',
     passport.authenticate('google')
   );
+
+  app.get(
+    '/auth/logout',
+    (req, res) => {
+      req.logout();
+      res.send(req.user);
+    }
+  );
+
+  app.get(
+    '/api/current_user',
+    (req, res) => {
+      res.send(req.user);
+    }
+  )
 };
 
 
