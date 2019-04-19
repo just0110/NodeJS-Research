bc47c4a496b11a1668262521a9691d107d47d095
hanna <hanna.likhtman@gmail.com>
2019-04-19 19:01:21 +0800

    fix  auth flow

--- services/passport.js
+++ services/passport.js
@@ -22,7 +22,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToke, profile, done) => {
       User.findOne({ googleId: profile.id })
